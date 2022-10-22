8683a8cc07384f8ae5844527f32435e909509c45
Jon Lambert <jonlambert@icloud.com>
2022-10-22 12:01:53 +0100

    feat: add vanilla extract / react component snippet

---
+++ index.ts
@@ -0,0 +1,55 @@
+import * as flags from 'https://deno.land/std@0.160.0/flags/mod.ts';
+import * as path from 'https://deno.land/std@0.160.0/path/mod.ts';
+import { z } from 'https://deno.land/x/zod@v3.19.1/mod.ts';
+import { camelCase } from 'https://deno.land/x/case@2.1.1/mod.ts';
+
+const ArgsSchema = z.object({
+  _: z.array(z.string()).length(1),
+  path: z.string().default('./'),
+  noChildren: z.boolean().default(false),
+});
+const args = ArgsSchema.safeParse(flags.parse(Deno.args));
+
+if (!args.success) {
+  console.error('usage: rve <component name>');
+  Deno.exit(0);
+}
+
+const componentName = args.data._[0];
+const componentCamelName = camelCase(componentName);
+
+const componentPath = path.join(args.data.path, `${componentName}.tsx`);
+const stylePath = path.join(args.data.path, `${componentName}.css.ts`);
+
+const addChildren = !args.data.noChildren;
+
+Deno.writeTextFileSync(
+  componentPath,
+  `
+import React from 'react';
+import { ${componentCamelName} } from './${componentName}.css';
+
+interface ${componentName}Props {
+  ${addChildren && `children: React.ReactNode;`}
+}
+
+export const ${componentName}: React.FC<${componentName}Props> = (${
+    addChildren ? '{ children }' : 'props'
+  }) => {
+  return <div className={${componentCamelName}}>${
+    addChildren && '{children}'
+  }</div>;
+}
+`
+);
+
+Deno.writeTextFileSync(
+  stylePath,
+  `
+import { style } from '@vanilla-extract/css';
+
+export const ${componentCamelName} = style({
+
+});
+`
+);
